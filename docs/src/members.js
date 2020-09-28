5677b9bf9de8b03a9e6ae6ba7a472052a6591f59
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-09-29 00:51:31 +0900

    メンバーごとに詳細内容を変える

--- docs/src/members.js
+++ docs/src/members.js
@@ -3,17 +3,75 @@ var members = new Vue({
   data:{
     upperRow: false,
     lowerRow: false,
-    imgSrc: "figs/members/default.png",
-    info: "info",
+    imgSrc: "",
+    name: "",
+    info: "",
   },
   methods:{
     clickedUpperImg: function(e){
+      switch (e.currentTarget.id){
+        case "tyanio":
+          this.imgSrc = "figs/members/default.png";
+          this.info = "";
+          break
+        case "kugi":
+          this.imgSrc = "figs/members/kugi.jpg";
+          this.info = `
+M2
+「スクラムマスター」のような立ち位置、プログラマー
+CG、ゲーム開発に興味があります
+`;
+          break;
+        case "yamakatsu":
+          this.imgSrc = "figs/members/yamakatsu.jpg";
+          this.info = `
+M1
+プログラマー
+フロントエンドを中心に、フルスタック目指してます
+`;
+          break;
+        case "inami":
+          this.imgSrc = "figs/members/default.png";
+          this.info = "";
+          break;
+        default:
+          this.imgSrc = "figs/members/default.png";
+          this.info = "";
+          break;
+      }
       console.log(e.currentTarget.id);
+      this.name = e.currentTarget.id;
       this.upperRow = true;
       this.lowerRow = false;
     },
     clickedLowerImg: function(e){
-      console.log(e.currentTarget.id);
+      switch (e.currentTarget.id){
+        case "daigo":
+          this.imgSrc = "figs/members/daigo.png";
+          this.info = `
+M1
+夢は、UIデザイナー
+好きな食べ物はお刺身
+心霊など、オカルト、好きです
+`;
+          break;
+        case "takeda":
+          this.imgSrc = "figs/members/default.png";
+          this.info = "";
+          break;
+        case "nishida":
+          this.imgSrc = "figs/members/nishida.jpg";
+          this.info = `
+B4
+したっぱ社員
+`;
+          break;
+        default:
+          this.imgSrc = "figs/members/default.png"
+          this.info = ""
+          break
+      }
+      this.name = e.currentTarget.id;
       this.upperRow = false;
       this.lowerRow = true;
     }
